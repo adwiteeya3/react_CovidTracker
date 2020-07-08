d2350f56eb3bb8316fe844c04fb2bdba68876a36
Adwiteeya <adwiteeya3@gmail.com>
2020-07-08 22:50:36 +0530

    final commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+//this file takes all the components and will export it from the components folder
+
+export { default as Cards } from './Cards/cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
